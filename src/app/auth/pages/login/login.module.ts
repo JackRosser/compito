00035a1193897623a7d0729c50e6f13317aae2b8
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-24 15:18:29 +0200

    ngmodel

--- src/app/auth/pages/login/login.module.ts
+++ src/app/auth/pages/login/login.module.ts
@@ -21,7 +21,7 @@ import { SharedModule } from '../../../shared/shared.module';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
-    ReactiveFormsModule,
+    FormsModule,
     SharedModule
   ]
 })
--- src/app/services/carloconti.service.ts
+++ src/app/services/carloconti.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,14 @@ import { Injectable } from '@angular/core';
 export class CarlocontiService {
 
   constructor() { }
+
+trollFace$ = new BehaviorSubject<boolean>(false)
+
+trollOn():void {
+  this.trollFace$.subscribe(bool => {
+    this.trollFace$.next(bool)
+  })
+}
+
+
 }
